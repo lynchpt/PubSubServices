1469d2273e70ee5575204eac0eb7849dc5b86556
Lynch, Patrick <patrick.lynch@outlook.com>
2019-03-25 10:07:30 -0400

    added PublisherService

---
+++ PubSubServices/PublisherService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PubSubServices.PublisherService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
